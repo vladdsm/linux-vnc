997d7f31048d552e3dc625aaee88faaf4ce62fe3
vladdsm <vladimir.zhbanko@gmail.com>
2019-05-12 22:24:02 +0200

    Update try_linux_ssl.sh

--- try_linux_ssl.sh
+++ try_linux_ssl.sh
@@ -1,4 +1,4 @@
 # run vnc session to docker container using ssl connection
 docker run -p 6081:443 -e SSL_PORT=443 -v ${PWD}/ssl:/etc/nginx/ssl -v /dev/shm:/dev/shm dorowu/ubuntu-desktop-lxde-vnc
 
-# access using localhost:6081
+# access using https://localhost:6081
